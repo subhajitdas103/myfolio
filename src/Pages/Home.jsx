5b00e1e12f947bbf79541a0b7ad314783d6de2ee
subhajit das <subhajitdas103@gmail.com>
2025-03-03 16:03:39 +0530

    up

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1></h1>
+      <p>I'm a passionate developer ready to create amazing things!</p>
+    </div>
+  );
+};
+
+export default Home;
